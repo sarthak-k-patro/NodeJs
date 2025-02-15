35b54384b7147479abb1f46c5fdcf7b366eefa45
Sarthak Kumar Patro <patrosarthak3@gmail.com>
2025-02-15 20:45:06 +0530

    simple routes added beginner

--- server/index.js
+++ server/index.js
@@ -2,10 +2,23 @@ const http = require("http");
 const fs = require("fs");
 const myServer = http.createServer((req, resp) => {
   console.log("new req received: ", req);
-  const log = `New Request received on: ${Date.now()} \n`;
-  fs.appendFile("log.txt", log, (err, data) => {
-    resp.end("Namaste From Server");
-  });
+  const log = `New Request received on: ${Date.now()} & at path : ${
+    req.url
+  } \n`;
+  fs.appendFile("log.txt", log, (err, data) => {});
+  switch (req.url) {
+    case "/":
+      resp.end("Namaste From Server");
+      break;
+    case "/home":
+      resp.end("HomePage");
+      break;
+    case "/about":
+      resp.end("Namaste I am Sarthak Patro");
+      break;
+    default:
+      resp.end("404 Not Found");
+  }
 });
 const port = 8003;
 myServer.listen(port, () => {
