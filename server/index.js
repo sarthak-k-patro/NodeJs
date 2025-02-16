722d375ed67c755af52d1c8132cda6a7073831be
Sarthak Kumar Patro <patrosarthak3@gmail.com>
2025-02-16 12:51:10 +0530

    using express and app.route

--- server/index.js
+++ server/index.js
@@ -1,26 +1,33 @@
 const http = require("http");
 const fs = require("fs");
-const myServer = http.createServer((req, resp) => {
-  console.log("new req received: ", req);
-  const log = `New Request received on: ${Date.now()} & at path : ${
-    req.url
-  } \n`;
-  fs.appendFile("log.txt", log, (err, data) => {});
-  switch (req.url) {
-    case "/":
-      resp.end("Namaste From Server");
-      break;
-    case "/home":
-      resp.end("HomePage");
-      break;
-    case "/about":
-      resp.end("Namaste I am Sarthak Patro");
-      break;
-    default:
-      resp.end("404 Not Found");
-  }
-});
+const express = require("express");
 const port = 8003;
-myServer.listen(port, () => {
+// const myServer = http.createServer((req, resp) => {
+//   console.log("new req received: ", req);
+//   const log = `New Request received on: ${Date.now()} & at path : ${
+//     req.url
+//   } \n`;
+//   fs.appendFile("log.txt", log, (err, data) => {});
+//   switch (req.url) {
+//     case "/":
+//       resp.end("Namaste From Server");
+//       break;
+//     case "/home":
+//       resp.end("HomePage");
+//       break;
+//     case "/about":
+//       resp.end("Namaste I am Sarthak Patro");
+//       break;
+//     default:
+//       resp.end("404 Not Found");
+//   }
+// });
+const app = express();
+app.route("/").get((req, res) => {
+  console.log("res and req shown in page");
+  res.json({ res: "Sarthak" });
+});
+
+app.listen(port, () => {
   console.log(`server started at: ${port}`);
 });
